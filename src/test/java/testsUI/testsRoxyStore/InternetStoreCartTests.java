1f266e9d0834532eca460237c373f51932846444
Vasenkov Stanislav <aanher@gmail.com>
2020-08-09 19:49:31 +0300

    Update and rename WorkWithInternetStoreCart.java to InternetStoreCartTests.java
    code review

--- src/test/java/testsUI/testsRoxyStore/InternetStoreCartTests.java
+++ src/test/java/testsUI/testsRoxyStore/InternetStoreCartTests.java
@@ -3,31 +3,29 @@ package testsUI.testsRoxyStore;
 import testsUI.Pages.InternetStorePage;
 import testsUI.core.SelenideBaseTest;
 import org.junit.jupiter.api.Test;
-//import org.junit.Test;
-
-public class WorkWithInternetStoreCart extends SelenideBaseTest {
 
 
-    /**
-     * Тест проверяет, что товар добавлен в корзину и добавлен именно выбранный товар
-     * 1. открываем страницу интернет-магазина
-     * 2. выбираю катагорию товаров
-     * 3. выбираю товар
-     * 4. добавляю товар в корзину
-     * 5. перехожу в корзину
-     * 6. проверяю, что в корзине нужный товар в нужном кол-ве
-     * Предусловие:
-     * кол-во товара в корзине = 1
-     * тест не параметризованный, все значения зашиты в код
-     */
+public class InternetStoreCartTests extends TestBase {
+
     @Test
-    public void testCheckAddProductToCart() {
-
-        InternetStorePage pageRoxyRussiaStore = new InternetStorePage("https://www.roxy-russia.ru/");
-        pageRoxyRussiaStore.openMainPageInternetStore();
-        pageRoxyRussiaStore.selectBikiniWearBottom();
-        pageRoxyRussiaStore.gotoCart();
-        pageRoxyRussiaStore.checkCountProduct();
+    @DisplayName("Проверка, что товар успешно добавляется в корзину")
+    @Description("1. открываем страницу интернет-магазина\n" +
+        "2. выбираю катагорию товаров\n" +
+        "3. выбираю товар\n" +
+        "4. добавляю товар в корзину\n" +
+        "5. перехожу в корзину\n" +
+        "6. проверяю, что в корзине нужный товар в нужном кол-ве\n" +
+        "Предусловие:\n" +
+        "кол-во товара в корзине = 1\n" +
+        "тест не параметризованный, все значения зашиты в код")
+    public void addProductToCartTest() {
+        InternetStorePage internetStorePage = new InternetStorePage();
+        open("https://www.roxy-russia.ru/");
+        
+        internetStorePage.selectBikiniWearBottom();
+        internetStorePage.gotoCart();
+        
+        internetStorePage.checkCountProduct();
     }
 
     /**
@@ -38,10 +36,13 @@ public class WorkWithInternetStoreCart extends SelenideBaseTest {
      */
     @Test
     public void testCheckSelectSize(){
-        InternetStorePage pageRoxyRussiaStore = new InternetStorePage("https://www.roxy-russia.ru/");
-        pageRoxyRussiaStore.openMainPageInternetStore();
-        pageRoxyRussiaStore.makeOrderWithoutSize();
-        pageRoxyRussiaStore.checkSelectSize();
+        InternetStorePage internetStorePage = new InternetStorePage();
+        open("https://www.roxy-russia.ru/");
+
+        internetStorePage.openMainPageInternetStore();
+        internetStorePage.makeOrderWithoutSize();
+        
+        internetStorePage.checkSelectSize();
     }
 
     /**
