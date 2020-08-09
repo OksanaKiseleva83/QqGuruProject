b7e5695c79844e4fce352e1e57ea2b12d8379a6e
Vasenkov Stanislav <aanher@gmail.com>
2020-08-09 19:48:50 +0300

    Update and rename SelenideBaseTest.java to TestBase.java
    code review

--- src/test/java/testsUI/core/TestBase.java
+++ src/test/java/testsUI/core/TestBase.java
@@ -8,37 +8,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.chrome.ChromeOptions;
 import com.codeborne.selenide.Selenide;
-//import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static org.openqa.selenium.remote.DesiredCapabilities.chrome;
 
 
-public class SelenideBaseTest {
-
-    /**
-     * конструктор по-умолчанию
-     */
-    public SelenideBaseTest() {
-        super();
-        return;
-    }
+public class TestBase {
 
     @BeforeEach
     public void setUp() {
-
-        final DesiredCapabilities capabilities = chrome();
         final ChromeOptions options = new ChromeOptions();
         options.addArguments("--lang=en_EN", "--disable-notifications", "--window-size=1920,1080", "--disable-system-timezone-automatic-detection", "--local-timezone");
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        capabilities.setPlatform(Platform.WINDOWS);
     }
 
     @AfterEach
     public void after() {
-        Selenide.clearBrowserCookies();
         closeWebDriver();
     }
-
 
 }
